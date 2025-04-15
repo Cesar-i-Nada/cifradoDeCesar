28be847432ef02c5a2811f2049533ae3d0773fc2
Cesar-i-Nada <cesar.alvarado.vega@gmail.com>
2025-04-15 00:07:54 -0600

    actualizacionParcial

--- Cifrado.py
+++ Cifrado.py
@@ -1,20 +1,36 @@
 class CifradoCesar:
-    def __init__(self, nombre_jugador):
+    def __init__(self, nombre):
       
-        self.nombre_jugador = nombre_jugador
+        self.nombre = nombre
         self.cifrando = False
 
     def iniciar_cifrado(self):
         self.cifrando = True
-        print(f"\n¡Bienvenido al juego, {self.nombre_jugador}!")
-        print("El juego ha comenzado..")
-        print("Aqui va el juego o los metodos de instancia donde vas a crear la logica del juego.")
+        print(f"\n¡Bienvenido al cifrador, {self.nombre}!")
+        print("El cifrado ha comenzado..")
+        print("Introduce el texto que deseas cifrar.")
+        
+        texto = input("texto: ")
+        if texto == texto.upper():
+            abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        else:
+            abc = "abcdefghijklmnopqrstuvwxyz"
+            
+            k = int(input("Valor de desplazamiento (k):"))
+            cifrado = ""
+            
+            for letra in texto:
+                if letra in abc:
+                    cifrado += abc[abc.index(letra) + k%(len(abc))]
+                else:
+                    cifrado += letra
+                    
+                    print("texto cifrado: ", cifrado)
 
-
-        print("¡Gracias por jugar!\n")
+        print("¡Gracias por cifrar!\n")
 
     def salir(self):
-        print(f"\nHasta luego, {self.nombre_jugador}. ¡Vuelve pronto!")
+        print(f"\nHasta luego, {self.nombre}. ¡Vuelve pronto!")
 
 
   
--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from Cifrado import CifradoCesar
 
 def mostrar_menu():
-    print("=== MENÚ PRINCIPAL ===")
-    print("1. Iniciar juego")
+    print("=== MENÚ PRINCIPAL DEL CIFRADOR ===")
+    print("1. Iniciar cifrado")
     print("2. Salir")
     print("======================")
 
 def main():
-    nombre = input("Ingresa un texto para cifrarlo: ")
-    juego = CifradoCesar(nombre)
+    nombre = input("Ingresa tu nombre y accede al menú principal: ")
+    cifrado = CifradoCesar(nombre)
     valor=True
     while valor:
         mostrar_menu()
         opcion = input("Selecciona una opción: ")
 
         if opcion == "1":
-            juego.iniciar_juego()
+            cifrado.iniciar_cifrado()
         elif opcion == "2":
-            juego.salir()
+            cifrado.salir()
             valor=False
         else:
             print("Opción inválida. Intenta de nuevo.\n")
