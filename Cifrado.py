fab790d9a63cbc310694e576a7f0cfeb5cc49db0
Cesar-i-Nada <cesar.alvarado.vega@gmail.com>
2025-04-15 21:51:52 -0600

    actualzaciónPreF

--- Cifrado.py
+++ Cifrado.py
@@ -1,13 +1,12 @@
 TAM_MAX_CLAVE = 26
-
 def obtenerModo():
-        while True:
-            print('¿Deseas encriptar o desencriptar un mensaje?')
-            modo = input().lower()
-            if modo in 'encriptar e desencriptar d'.split():
-                return modo
-            else:
-                print('Ingresa "encriptar" o "e" o "desencriptar" o "d"')
+    while True:
+        print('¿Deseas encriptar o desencriptar un mensaje?')
+        modo = input().lower()
+        if modo in 'encriptar e desencriptar d'.split():
+            return modo
+        else:
+            print('Ingresa "encriptar" o "e" o "desencriptar" o "d"')
 
 def obtenerMensaje():
     print('Ingresa tu mensaje:')
@@ -23,34 +22,32 @@ def obtenerClave():
 
 def obtenerMensajeTraducido(modo, mensaje, clave):
     if modo[0] == 'd':
-      clave= -clave
+        clave= -clave
     traduccion = ''
 
     for simbolo in mensaje:
-      if simbolo.isalpha():
-        num = ord(simbolo)
-        num += clave
+        if simbolo.isalpha():
+            num = ord(simbolo)
+            num += clave
 
-        if simbolo.isupper():
-            if num > ord('Z'):
-                num -= 26
-            elif num < ord('A'):
-                num += 26
-        elif simbolo.islower():
-            if num > ord('z'):
-                num -= 26
-            elif num < ord('a'):
-                num += 26
+            if simbolo.isupper():
+                if num > ord('Z'):
+                    num -= 26
+                elif num < ord('A'):
+                    num += 26
+            elif simbolo.islower():
+                if num > ord('z'):
+                    num -= 26
+                elif num < ord('a'):
+                    num += 26
 
-        traduccion += chr(num)
-      else:
-        traduccion += simbolo
+            traduccion += chr(num)
+        else:
+            traduccion += simbolo
     return traduccion
-
 modo = obtenerModo()
 mensaje = obtenerMensaje()
 clave = obtenerClave()
 
 print('Tu texto traducido es:')
 print(obtenerMensajeTraducido(modo, mensaje, clave))
-
